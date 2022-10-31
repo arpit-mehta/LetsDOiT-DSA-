8d1a9a45eb4d51f61aa43aef5aaf94544bee77c1
arpit-mehta <91524040+arpit-mehta@users.noreply.github.com>
2022-10-31 12:37:57 +0530

    Update BinarySearch.py

--- Binary Search/BinarySearch.py
+++ Binary Search/BinarySearch.py
@@ -1,18 +1,18 @@
 def binarysearch(arr, n, k):
-		start=0
-		end=n-1
+	start=0
+	end=n-1
     
     #this gives the index of the k element we are searching for.
-		ans=-1;
+	ans=-1;
 		
-    while start<=end:
-		    mid=((start+end)//2)
+    	while start<=end:
+		mid=((start+end)//2)
 		    
-		    if arr[mid]==k:
-		        ans=mid
+		if arr[mid]==k:
+			ans=mid
 		        return ans
-		    elif arr[mid]<k:
+		elif arr[mid]<k:
 		        start=mid+1
-		    else:
+		else:
 		        end=mid-1
-    return ans
+    	return ans
